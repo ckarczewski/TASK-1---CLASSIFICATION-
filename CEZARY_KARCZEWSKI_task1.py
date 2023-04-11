9ff7df02a4f65f9ec538dce7bf02da47f1a81e5c
Czarek Karczewski <keracz1997@gmail.com>
2023-04-11 19:04:58 +0200

    Load data

--- CEZARY_KARCZEWSKI_task1.py
+++ CEZARY_KARCZEWSKI_task1.py
@@ -10,3 +10,8 @@ import pathlib
 import matplotlib.pyplot as plt
 
 from tensorflow.keras.metrics import Precision, Recall, Accuracy
+
+data_dir = "/home/cezary/Documents/GoOnline/data/"
+data_dir = pathlib.Path(data_dir)
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print("img count",image_count)
