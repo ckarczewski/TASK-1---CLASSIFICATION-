26550683e1ee30f101e8ef70e74bd6fa914ebb9e
Czarek Karczewski <keracz1997@gmail.com>
2023-04-11 19:21:07 +0200

    creating functions to split data

--- CEZARY_KARCZEWSKI_task1.py
+++ CEZARY_KARCZEWSKI_task1.py
@@ -39,3 +39,26 @@ dataset = dataset.map(lambda x, y: (x/255, y))
 data_augmentation = keras.Sequential([
     layers.RandomFlip("horizontal", input_shape=(300,300,3))
 ])
+
+# Data splitting
+def data_split(dataset, 
+               train_split=0.8, 
+               val_split=0.1, 
+               test_split=0.1, 
+               shuffle=True, 
+               shuffle_size=10000):
+    assert (train_split + val_split + test_split) == 1
+    
+    if shuffle:
+        dataset = dataset.shuffle(shuffle_size, seed=123)
+    
+    train_size = int(len(dataset)*.8)
+    val_size = int(len(dataset)*.1)
+
+    train_dataset = dataset.take(train_size)
+    val_dataset = dataset.skip(train_size).take(val_size)
+    test_dataset = dataset.skip(train_size).skip(val_size)
+
+    return train_dataset, val_dataset, test_dataset
+
+train_dataset, val_dataset, test_dataset = data_split(dataset=dataset)
