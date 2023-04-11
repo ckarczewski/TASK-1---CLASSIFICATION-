c7f494f87ceed545dd19af9b3febaacee6a3b776
Czarek Karczewski <keracz1997@gmail.com>
2023-04-11 19:08:03 +0200

    adding labels

--- CEZARY_KARCZEWSKI_task1.py
+++ CEZARY_KARCZEWSKI_task1.py
@@ -1,7 +1,7 @@
 import os
 
 import PIL 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -11,7 +11,23 @@ import matplotlib.pyplot as plt
 
 from tensorflow.keras.metrics import Precision, Recall, Accuracy
 
+# Load data
 data_dir = "/home/cezary/Documents/GoOnline/data/"
 data_dir = pathlib.Path(data_dir)
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print("img count",image_count)
+
+batch_size = 32
+img_height = 300
+img_width = 300
+
+# Adding labels
+dataset = tf.keras.utils.image_dataset_from_directory(
+    directory=data_dir,
+    labels='inferred', 
+    label_mode='int',
+    image_size=(img_height, img_width),
+    batch_size=batch_size,
+)
+class_names = dataset.class_names
+print(class_names)
